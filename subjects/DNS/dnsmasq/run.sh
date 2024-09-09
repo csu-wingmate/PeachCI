88c96e7a64829b6a75ab0abfa99a556c00b996b2
wanli0 <141891945+wanli0@users.noreply.github.com>
2024-09-09 20:05:02 +0800

    Update run.sh

--- subjects/DNS/dnsmasq/run.sh
+++ subjects/DNS/dnsmasq/run.sh
@@ -27,6 +27,5 @@ python3 /root/collect.py ${cov_edge_path} \
 
 # Peach 模糊测试的路径
 export LUCKY_GLOBAL_MMAP_FILE=${cov_edge_path} SHM_ENV_VAR=${cov_bitmap_path} 
-/root/LightFTP/Source/Release/fftp /root/LightFTP/Bin/fftp.conf 
-
+/usr/local/sbin/dnsmasq -p 5353 -d -C /root/dns/conf/dnsmasq.conf -8 /var/log/dnsmasq/dnsmasq.log
 while true; do echo 'Worker: Hit CTRL+C'; sleep 1800; done
