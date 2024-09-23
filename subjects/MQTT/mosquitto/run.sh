2a354bee26382b66b90c012a5529b9339fd4b770
wanlichen <141891945+wanli0@users.noreply.github.com>
2024-09-23 14:48:34 +0800

    Create run.sh

---
+++ subjects/MQTT/mosquitto/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+FUZZER=$1
+# 协议和项目名称
+protocol=mqtt
+project=mosquitto
+port=4489
+
+# 当前时间
+ttime=`date +%Y-%m-%d-%T`
+t="${FUZZER}_mqtt-${ttime}"
+
+# 创建临时文件路径
+cov_edge_path="/dev/shm/edge_${t}"
+cov_bitmap_path="/dev/shm/bitmap_${t}"
+
+# 创建临时文件
+dd if=/dev/zero of=${cov_edge_path}  bs=10M count=1
+dd if=/dev/zero of=${cov_bitmap_path} bs=10M count=1
+export LUCKY_GLOBAL_MMAP_FILE=${cov_edge_path}
+
+# 创建临时目录
+mkdir branch
+
+# 运行收集器
+python3 /root/collect.py ${cov_edge_path} \
+    "./branch/collect_branch_mutable_${project}_${t}_${port}" &
+
+# Peach 模糊测试的路径
+export LUCKY_GLOBAL_MMAP_FILE=${cov_edge_path} SHM_ENV_VAR=${cov_bitmap_path} 
+/root/mosquitto/src/mosquitto -p 4489
+while true; do echo 'Worker: Hit CTRL+C'; sleep 1800; done
