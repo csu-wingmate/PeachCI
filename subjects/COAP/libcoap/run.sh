e75413d8702a7a17539f1bb1aea778f19d648bbe
wanlichen <141891945+wanli0@users.noreply.github.com>
2024-10-15 16:41:35 +0800

    Update run.sh

--- subjects/COAP/libcoap/run.sh
+++ subjects/COAP/libcoap/run.sh
@@ -3,8 +3,7 @@ FUZZER=$1
 # 协议和项目名称
 protocol=coap
 project=libcoap
-port=21577
-
+port=6777
 # 当前时间
 ttime=`date +%Y-%m-%d-%T`
 t="${FUZZER}_coap-${ttime}"
@@ -27,5 +26,5 @@ python3 /root/collect.py ${cov_edge_path} \
 
 # Peach 模糊测试的路径
 export LUCKY_GLOBAL_MMAP_FILE=${cov_edge_path} SHM_ENV_VAR=${cov_bitmap_path} 
-/root/libcoap/examples/coap-server -A 127.0.0.1 -p 6777 -e -d 100 -v 9
+/root/libcoap/examples/coap-server  -p 6777 -e -d 100 -v 9
 while true; do echo 'Worker: Hit CTRL+C'; sleep 1800; done
