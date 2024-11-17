51ca99ca5d60377ba217981fc153574edfeccbb7
wanlichen <141891945+wanli0@users.noreply.github.com>
2024-11-17 15:05:14 +0800

    Update profuzzpeach_build_all.sh

--- scripts/execution/profuzzpeach_build_all.sh
+++ scripts/execution/profuzzpeach_build_all.sh
@@ -12,13 +12,29 @@ cd fuzzer/PeachStar
 docker build . -t peachstar --build-arg MAKE_OPT $NO_CACHE
 
 cd $PFBENCH
+cd fuzzer/Charon
+docker build . -t charon --build-arg MAKE_OPT $NO_CACHE
+
+cd $PFBENCH
 cd subjects/FTP/lightftp
 docker build . -t lightftp --build-arg MAKE_OPT $NO_CACHE
 
 cd $PFBENCH
-cd subjects/HTTP/apachehttpd
-docker build . -t apachehttpd --build-arg MAKE_OPT $NO_CACHE
-
-cd $PFBENCH
 cd subjects/DNS/dnsmasq
 docker build . -t dnsmasq --build-arg MAKE_OPT $NO_CACHE
+
+cd $PFBENCH
+cd subjects/COAP/libcoap
+docker build . -t libcoap --build-arg MAKE_OPT $NO_CACHE
+
+cd $PFBENCH
+cd subjects/DDS/cyclonedds
+docker build . -t cyclonedds --build-arg MAKE_OPT $NO_CACHE
+
+cd $PFBENCH
+cd subjects/TLS/openssl
+docker build . -t openssl --build-arg MAKE_OPT $NO_CACHE
+
+cd $PFBENCH
+cd subjects/HTTP/apachehttpd
+docker build . -t apachehttpd --build-arg MAKE_OPT $NO_CACHE
