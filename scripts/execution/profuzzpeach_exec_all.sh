4adda4b685478b615afaba8cd8267b8aed8c3eea
wanlichen <141891945+wanli0@users.noreply.github.com>
2024-11-17 15:11:13 +0800

    Update profuzzpeach_exec_all.sh

--- scripts/execution/profuzzpeach_exec_all.sh
+++ scripts/execution/profuzzpeach_exec_all.sh
@@ -39,15 +39,122 @@ do
 
             if [[ $FUZZER == "peach" ]] || [[ $FUZZER == "all" ]]
             then
-                profuzzbench_exec_common.sh lightftp $NUM_CONTAINERS results-lightftp peach $TIMEOUT 
+                profuzzbench_exec_common.sh lightftp $NUM_CONTAINERS results-lightftp peach $TIMEOUT 1
             fi
 
             if [[ $FUZZER == "peachstar" ]] || [[ $FUZZER == "all" ]]
             then
-                profuzzbench_exec_common.sh lightftp $NUM_CONTAINERS results-lightftp peachstar $TIMEOUT -pro
+                profuzzbench_exec_common.sh lightftp $NUM_CONTAINERS results-lightftp peachstar $TIMEOUT 1
+            fi
+            if [[ $FUZZER == "charon" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh lightftp $NUM_CONTAINERS results-lightftp charon $TIMEOUT 1
             fi
 
         fi
+
+
+##### DNS #####
+
+        if [[ $TARGET == "dnsmasq" ]] || [[ $TARGET == "all" ]]
+        then
+
+            cd $PFBENCH
+            mkdir results-dnsmasq
+
+            if [[ $FUZZER == "peach" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh dnsmasq $NUM_CONTAINERS results-dnsmasq peach $TIMEOUT 1
+            fi
+
+            if [[ $FUZZER == "peachstar" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh dnsmasq $NUM_CONTAINERS results-dnsmasq peachstar $TIMEOUT 1
+            fi
+            if [[ $FUZZER == "charon" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh dnsmasq $NUM_CONTAINERS results-dnsmasq charon $TIMEOUT 1
+            fi
+
+        fi
+
+
+
+##### DDS #####
+
+        if [[ $TARGET == "cyclonedds" ]] || [[ $TARGET == "all" ]]
+        then
+
+            cd $PFBENCH
+            mkdir results-cyclonedds
+
+            if [[ $FUZZER == "peach" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh cyclonedds $NUM_CONTAINERS results-cyclonedds peach $TIMEOUT 1
+            fi
+
+            if [[ $FUZZER == "peachstar" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh cyclonedds $NUM_CONTAINERS results-cyclonedds peachstar $TIMEOUT 1
+            fi
+            if [[ $FUZZER == "charon" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh cyclonedds $NUM_CONTAINERS results-cyclonedds charon $TIMEOUT 1
+            fi
+
+        fi
+
+
+        
+
+##### COAP #####
+
+        if [[ $TARGET == "libcoap" ]] || [[ $TARGET == "all" ]]
+        then
+
+            cd $PFBENCH
+            mkdir results-libcoap
+
+            if [[ $FUZZER == "peach" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh libcoap $NUM_CONTAINERS results-libcoap peach $TIMEOUT 1
+            fi
+
+            if [[ $FUZZER == "peachstar" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh libcoap $NUM_CONTAINERS results-libcoap peachstar $TIMEOUT 1
+            fi
+            if [[ $FUZZER == "charon" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh libcoap $NUM_CONTAINERS results-libcoap charon $TIMEOUT 1
+            fi
+
+        fi
+
+##### TLS #####
+
+        if [[ $TARGET == "openssl" ]] || [[ $TARGET == "all" ]]
+        then
+
+            cd $PFBENCH
+            mkdir results-openssl
+
+            if [[ $FUZZER == "peach" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh openssl $NUM_CONTAINERS results-openssl peach $TIMEOUT 1
+            fi
+
+            if [[ $FUZZER == "peachstar" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh openssl $NUM_CONTAINERS results-openssl peachstar $TIMEOUT 1
+            fi
+            if [[ $FUZZER == "charon" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh openssl $NUM_CONTAINERS results-openssl charon $TIMEOUT 1
+            fi
+
+        fi
+
 
 ##### HTTP #####
 
@@ -59,12 +166,16 @@ do
 
             if [[ $FUZZER == "peach" ]] || [[ $FUZZER == "all" ]]
             then
-                profuzzbench_exec_common.sh apachehttpd $NUM_CONTAINERS results-apachehttpd peach $TIMEOUT 
+                profuzzbench_exec_common.sh apachehttpd $NUM_CONTAINERS results-apachehttpd peach $TIMEOUT 1
             fi
 
             if [[ $FUZZER == "peachstar" ]] || [[ $FUZZER == "all" ]]
             then
-                profuzzbench_exec_common.sh apachehttpd $NUM_CONTAINERS results-apachehttpd peachstar $TIMEOUT -pro
+                profuzzbench_exec_common.sh apachehttpd $NUM_CONTAINERS results-apachehttpd peachstar $TIMEOUT 1
+            fi
+            if [[ $FUZZER == "charon" ]] || [[ $FUZZER == "all" ]]
+            then
+                profuzzbench_exec_common.sh apachehttpd $NUM_CONTAINERS results-apachehttpd charon $TIMEOUT 1
             fi
 
         fi
