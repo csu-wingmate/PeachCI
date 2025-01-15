1b3c848116dd9c53c188c82015941780329f92b5
wanlichen <141891945+wanli0@users.noreply.github.com>
2025-01-15 14:55:29 +0800

    Update build_all.sh

--- scripts/execution/build_all.sh
+++ scripts/execution/build_all.sh
@@ -3,38 +3,38 @@
 #export NO_CACHE="--no-cache"
 #export MAKE_OPT="-j4"
 
-cd $PFBENCH
+cd $CIPATH
 cd fuzzer/Peach
 docker build . -t peach --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd fuzzer/PeachStar
 docker build . -t peachstar --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd fuzzer/Charon
 docker build . -t charon --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd subjects/FTP/lightftp
 docker build . -t lightftp --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd subjects/DNS/dnsmasq
 docker build . -t dnsmasq --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd subjects/COAP/libcoap
 docker build . -t libcoap --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd subjects/DDS/cyclonedds
 docker build . -t cyclonedds --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd subjects/TLS/openssl
 docker build . -t openssl --build-arg MAKE_OPT $NO_CACHE
 
-cd $PFBENCH
+cd $CIPATH
 cd subjects/HTTP/apachehttpd
 docker build . -t apachehttpd --build-arg MAKE_OPT $NO_CACHE
