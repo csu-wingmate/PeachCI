7f6869df247e363086010a68757191a71eff9869
wanlichen <141891945+wanli0@users.noreply.github.com>
2025-01-16 15:47:15 +0800

    Update run.sh

--- fuzzers/Peach/run.sh
+++ fuzzers/Peach/run.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
 TIMEOUT=$1
 PROTOCOL=$2
 i=$3
+OPTION=$4
 
-timeout ${TIMEOUT} mono /root/Peach/bin/peach.exe /root/tasks/${PROTOCOL}_run_${i}.xml &
+if [[ $OPTION == -p* ]]; then
+    work_num=$(echo "$OPTION" | cut -d' ' -f2)
+    for j in $(seq 1${work_num}) ; do
+        timeout ${TIMEOUT} mono /root/Peach/bin/peach.exe \
+        /root/tasks/${PROTOCOL}_run_${i}.xml -p${work_num},${j} &
+        sleep 1
+    done
+else
+    # 如果OPTION不是以"-p"开头，则执行Peach Fuzzer
+    timeout ${TIMEOUT} mono /root/Peach/bin/peach.exe${OPTION} /root/tasks/${PROTOCOL}_run_${i}.xml &
+fi
 
-while true; do echo 'Worker: Hit CTRL+C'; sleep 1800; done
+# 无限循环，可以按CTRL+C退出
+while true; do
+    echo 'Worker: Hit CTRL+C to exit'
+    sleep 1800
+done
+
