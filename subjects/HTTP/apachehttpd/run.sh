8f2663151040bfcbd27cbea1ed679f9737e568c8
wanlichen <141891945+wanli0@users.noreply.github.com>
2025-01-16 16:15:39 +0800

    Update run.sh

--- subjects/HTTP/apachehttpd/run.sh
+++ subjects/HTTP/apachehttpd/run.sh
@@ -19,11 +19,13 @@ dd if=/dev/zero of=${cov_bitmap_path} bs=10M count=1
 export LUCKY_GLOBAL_MMAP_FILE=${cov_edge_path}
 
 # 创建临时目录
-mkdir branch
+mkdir /root/branch
 
 # 运行收集器
 python3 /root/collect.py ${cov_edge_path} \
-    "./branch/collect_branch_mutable_${project}_${t}_${port}" &
+    "/root/branch/${FUZZER}_branch_${project}_${t}_${port}" &
+    
+python3 /root/collect_prometheus.py ${cov_edge_path}  "/root/branch/${FUZZER}_branch_${project}_${t}_${port}.prom" ${FUZZER} ${project} &
 
 # Peach 模糊测试的路径
 export LUCKY_GLOBAL_MMAP_FILE=${cov_edge_path} SHM_ENV_VAR=${cov_bitmap_path} 
