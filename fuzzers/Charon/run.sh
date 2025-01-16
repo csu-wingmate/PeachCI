62ff2eea16cc95cdae6aa73ace72d1962f425877
wanlichen <141891945+wanli0@users.noreply.github.com>
2025-01-16 15:50:54 +0800

    Update run.sh

--- fuzzers/Charon/run.sh
+++ fuzzers/Charon/run.sh
@@ -2,11 +2,27 @@
 TIMEOUT=$1
 PROTOCOL=$2
 i=$3
+OPTION=$4
+
 cd /dev/shm
 dd if=/dev/zero bs=10M count=1 of=$name-of-shared-memory
 export LD_LIBRARY_PATH=/root/Charon/:$LD_LIBRARY_PATH
 export SHM_ENV_VAR=/dev/shm/$name-of-shared-memory
 
-timeout ${TIMEOUT} mono /root/Charon/executable/charon.exe /root/tasks/${PROTOCOL}_run_${i}.xml &
+if [[ $OPTION == -p* ]]; then
+    work_num=$(echo "$OPTION" | cut -d' ' -f2)
+    for j in $(seq 1${work_num}) ; do
+        timeout ${TIMEOUT} mono /root/Charon/executable/charon.exe \
+        /root/tasks/${PROTOCOL}_run_${i}.xml -p${work_num},${j} &
+        sleep 1
+    done
+else
+    # 如果OPTION不是以"-p"开头，则执行Charon
+    timeout ${TIMEOUT} mono /root/Charon/executable/charon.exe ${OPTION} /root/tasks/${PROTOCOL}_run_${i}.xml &
+fi
 
-while true; do echo 'Worker: Hit CTRL+C'; sleep 1800; done
+# 无限循环，可以按CTRL+C退出
+while true; do
+    echo 'Worker: Hit CTRL+C to exit'
+    sleep 1800
+done
